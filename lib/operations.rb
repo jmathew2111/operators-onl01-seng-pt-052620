ee87c242574a2f48f81b6dc9d6dee9bf7cd59e69
Justin Mathew <jmathew2111@yahoo.com>
2020-06-09 22:53:27 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
-def unsafe?(speed)
-
+def unsafe?(mph)
+  if mph < 40
+    return true
+  elsif mph > 60  
+   return true
+ elsif mph > 40 
+   return false
+  else mph < 60
+    return false
+  end
 end
 
-
-
-def not_safe?(speed)
-	
-end
-	
-
-
+def not_safe?(mph)
+  mph > 40 ? false : true
+  mph < 60 ? false : true 
+ mph < 40 || mph > 60 ? true : false
+end 
