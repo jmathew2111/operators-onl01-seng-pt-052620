2feaaf896ce85ac9fc7fc95407c3bbe0b6512707
Justin Mathew <jmathew2111@yahoo.com>
2020-06-09 22:10:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,18 @@
 def unsafe?(mph)
   if mph < 40
-    return "true"
+    return true
   elsif mph > 60  
-   return "true"
- elsif mph > 40 == "Hi!"
-   return "FALSE"
+   return true
+ elsif mph > 40 
+   return false
   else mph < 60
-    return "False"
+    return false
   end
 end
 
 def no_safe?(mph)
-  mph < 40 ? "true":"false"
-  mph > 60 ? "true":"false"
-  mph > 40 ? "false" :"false"
-  mph < 60 ? "false": "false"
+  mph < 40 ? true : false
+  mph > 60 ? true : false
+  mph > 40 ? false : false
+  mph < 60 ? false : false
 end 
